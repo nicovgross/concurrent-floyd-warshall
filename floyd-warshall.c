95ae736e9ec422ec13336c0916db6600e0bbade1
nicovgross <nicholasvallegross@gmail.com>
2025-06-01 23:58:32 -0300

    Add time tracking and output file support

--- floyd-warshall.c
+++ floyd-warshall.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <stdint.h>
+#include <time.h>
 #include "leGrafo.h"
 
 //variáveis globais
-int *dist_curr;
-int *dist_prev;
-int V, E;
-int nthreads;
+int *dist_curr; //matriz de distâncias da iteração atual
+int *dist_prev; //matriz de distâncias da iteração anterior
+int V, E; //Número de vértices e arestas
+int nthreads; //número de threads
 
 //variáveis de sincronização
 pthread_mutex_t mutex;
@@ -36,6 +37,11 @@ void barreira(int nthreads) {
     pthread_mutex_unlock(&mutex); //fim secao critica
 }
 
+/*
+Algoritmo do Floyd-Warshall
+Recebe o id da thread e com base nisso calcula as respectivas linhas da matriz de distância,
+sempre consultando a matriz de distâncias da iteração anterior para calcular os valores da matriz atual
+*/
 void* Floyd_Warshall(void *arg) {
     int id = (intptr_t) arg;
     
@@ -56,12 +62,16 @@ void* Floyd_Warshall(void *arg) {
 
 int main(int argc, char* argv[]) {
     pthread_t *tid;
+    struct timespec inicio, fim;
+    double temp_exec;
+    FILE *arq;
+    size_t ret;
 
     pthread_mutex_init(&mutex, NULL);
     pthread_cond_init (&cond, NULL);
 
-    if(argc < 3) {
-        fprintf(stderr, "Digite: %s <numero de threads> <arquivo de entrada>\n", argv[0]);
+    if(argc < 4) {
+        fprintf(stderr, "Digite: %s <numero de threads> <arquivo de entrada> <arquivo de saída>\n", argv[0]);
         return 1;
     }
     nthreads = atoi(argv[1]);
@@ -71,18 +81,18 @@ int main(int argc, char* argv[]) {
         return 2;
     }
 
-    leGrafo(&dist_curr, &V, &E, argv[2]);
-    printf("Matriz de entrada:\n\n");
-    imprime_grafo(V, E, dist_curr);
-    printf("\n\n");
+    leGrafo(&dist_curr, &V, &E, argv[2]); //lê o grafo do arquivo de entrada
     dist_prev = (int*) malloc(sizeof(int) * V*V);
     copia_matriz(dist_curr, dist_prev, V);
     
+    clock_gettime(CLOCK_MONOTONIC, &inicio);
+
     //Cria as threads
     for(int id=0; id<nthreads; id++) {
         pthread_create(&tid[id], NULL, Floyd_Warshall, (void*)(intptr_t)id);
     }
 
+    //Espera todas as threads completarem
     for (int id=0; id<nthreads; id++) {
         if (pthread_join(tid[id], NULL)) {
             printf("--ERRO: pthread_join() \n"); 
@@ -90,10 +100,18 @@ int main(int argc, char* argv[]) {
         } 
     } 
 
-    printf("Matriz de saída:\n\n");
-    imprime_grafo(V, E, dist_curr);
+    clock_gettime(CLOCK_MONOTONIC, &fim);
+    temp_exec = (fim.tv_sec - inicio.tv_sec) + (fim.tv_nsec - inicio.tv_nsec) / 1e9;
 
-    //Espera todas as threads completarem
+    arq = fopen(argv[3], "wb");
+    if(!arq) { fprintf(stderr, "Erro de abertura do arquivo de saída\n"); return 3; }
+
+    ret = fwrite(&temp_exec, sizeof(int), 1, arq);
+    if(!ret) { fprintf(stderr, "Erro na escrita do arquivo de saída\n"); return 4; }
+    ret = fwrite(dist_curr, sizeof(int), V*V, arq);
+    if(!ret) { fprintf(stderr, "Erro na escrita do arquivo de saída\n"); return 4; }
+
+    //Libera a memória alocada
     free(tid);
     free(dist_curr);
     free(dist_prev); 
